7ba19b485adc92fcad34a32453a5b85fc9b92924
onesol98 <71314689+hansolbangul@users.noreply.github.com>
2023-03-21 19:08:39 +0900

    FEAT: create data fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "./page.module.css";
-
 export const metadata = {
   title: "IMDB Clone",
   description: "The IMDB Clone coding",
 };
 
-export default function Home() {
+type Props = {
+  searchParams: any;
+};
+
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }: Props) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(`${BASE_URL}${genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"}?api_key=${API_KEY}&language=ko-KR&page=1`, {
+    next: { revalidate: 10000 },
+  });
+
+  const data = await res.json();
+
+  const results = data.results;
+
   return <h1 className="text-red-400">Home</h1>;
 }
