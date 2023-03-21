dc661c74dd784995e18304d8718ebc1e9de7be8f
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2023-03-21 22:51:10 +0900

    FEAT: handle the error

---
+++ src/app/error.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useEffect } from "react";
+
+type Props = {
+  error: Error;
+  reset: () => void;
+};
+
+export default function Error({ error, reset }: Props) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+  return (
+    <div className="text-center mt-10 ">
+      <h1>Something went wrong</h1>
+      <button className="hover:text-amber-600" onClick={() => reset()}>
+        try Again
+      </button>
+    </div>
+  );
+}
