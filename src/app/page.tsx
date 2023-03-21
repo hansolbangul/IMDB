ba81e57152646a43ae87761a5a19e810cf559f91
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2023-03-21 23:37:22 +0900

    FEAT: add loading effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Movie } from "@/types/movie";
 import Result from "./components/Result";
 
 export const metadata = {
@@ -12,7 +13,7 @@ type Props = {
 const BASE_URL = "https://api.themoviedb.org/3/";
 const API_KEY = process.env.API_KEY;
 
-export default async function Home({ searchParams }: Props) {
+const getFetch = async ({ searchParams }: Props): Promise<Movie[]> => {
   const genre = searchParams.genre || "fetchTrending";
   const res = await fetch(
     `${BASE_URL}${
@@ -23,13 +24,13 @@ export default async function Home({ searchParams }: Props) {
     }
   );
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
   const data = await res.json();
 
-  const results = data.results;
+  return data.results;
+};
+
+export default async function Home({ searchParams }: Props) {
+  const results = await getFetch({ searchParams });
 
   return (
     <div>
